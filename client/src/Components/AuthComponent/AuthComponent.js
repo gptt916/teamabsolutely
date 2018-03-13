86df59241f3ba5fac9f0b24a0704f87988a17baf
gobingiri <gobingiritharan@gmail.com>
2018-03-13 16:53:46 -0400

    front-end FB auth, rough imp

---
+++ client/src/Components/AuthComponent/AuthComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class AuthComponent extends Component {
+
+    componentDidMount(){
+        window.fbAsyncInit = function() {
+            window.FB.init({
+                appId   : '201716760590344',
+                cookie  : true,
+                xfbml   : true,
+                version : 'v2.1'
+            });
+
+            window.FB.Event.subscribe('auth.statusChange', (response) => {
+                if (response.authResponse) {
+                    this.updateLoggedInState(response)
+                } else {
+                    this.updateLoggedOutState()
+                }
+            });
+        }.bind(this);
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+
+        }(document, 'script', 'facebook-jssdk'));
+    }
+
+    render() {
+        return (
+            <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+        );
+    }
+}
--- client/src/Containers/App.js
+++ client/src/Containers/App.js
@@ -3,21 +3,22 @@ import classes from './App.css';
 import Persons from '../Components/Persons/Persons';
 import Cockpit from '../Components/Cockpit/Cockpit';
 import Navbar from '../Components/NavbarComponents/Navbar/Navbar';
+import AuthComponent from '../Components/AuthComponent/AuthComponent';
 import { connect } from 'react-redux';
 import * as actionTypes from '../Stores/actions';
 
 class App extends Component {
   	render() {
-			return (
-				<div className={classes.App}>
-					<header>
-						<Navbar
-							navbarItems={this.props.navbarItems}
-						/>
-					</header>
-				</div>
-			);
-  }
+        return (
+            <div className={classes.App}>
+                <header>
+                    <Navbar
+						navbarItems={this.props.navbarItems}
+					/>
+                    <AuthComponent/>
+				</header>
+			</div>);
+    }
 }
 
 const mapStateToProps = (state) => {
