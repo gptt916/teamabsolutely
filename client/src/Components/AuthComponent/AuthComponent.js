b8f513a33f69fe33ef951b1a327f8195991cd732
gobingiri <gobingiritharan@gmail.com>
2018-03-13 17:34:28 -0400

    supressed attribute name error

--- client/src/Components/AuthComponent/AuthComponent.js
+++ client/src/Components/AuthComponent/AuthComponent.js
@@ -32,7 +32,7 @@ export default class AuthComponent extends Component {
 
     render() {
         return (
-            <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+            <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
         );
     }
 }
