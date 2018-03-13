8bf00f2384448fd9263afed7932ffc3f97d52c0b
Nick G <gptt916@hotmail.com>
2018-03-13 15:47:25 -0400

    sidebar working, without previous navbar

--- client/src/Components/SidebarComponents/Sidebar/Sidebar.js
+++ client/src/Components/SidebarComponents/Sidebar/Sidebar.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import classes from './Sidebar.css';
-import SidebarItems from '../SidebarItems/SidebarItems';
 
-const sidebar = (props) => {
-    return (
-        <div className={classes.container}>
-            {/* <SidebarItems sidebarItems={props.sidebarItems}/> */}
-            <p>Something Something</p>
-        </div>
-    );
-};
-
-export default sidebar;
+export default Sidebar;
 
